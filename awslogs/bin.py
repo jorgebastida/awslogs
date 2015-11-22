23147bbeb1d078bd101aa82908164fbf6064ba51
Jorge Bastida <me@jorgebastida.com>
2015-11-22 09:59:57 +0000

    Bump version

--- awslogs/bin.py
+++ awslogs/bin.py
@@ -10,7 +10,7 @@ from . import exceptions
 from .core import AWSLogs
 
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 
 def main(argv=None):
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name='awslogs',
-    version='0.1.2',
+    version='0.2.0',
     url='http://github.com/jorgebastida/awslogs',
     license='BSD',
     author='Jorge Bastida',
