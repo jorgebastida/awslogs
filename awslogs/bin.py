e36ca69ef6b0533ad1f022947fe3e1bf57e0719c
Jorge Bastida <me@jorgebastida.com>
2016-02-10 21:03:02 +0000

    Correctly wrap sys.stdout into an instance of StreamWriter so we can print utf-8 correctly

--- awslogs/bin.py
+++ awslogs/bin.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import locale
+import codecs
 import argparse
 
 import boto3
@@ -14,6 +16,7 @@ __version__ = "0.3.0"
 
 
 def main(argv=None):
+    sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
     argv = (argv or sys.argv)[1:]
 
