af17cc069363bad7ee5f46e2a5382f51473bc8c6
Nathan Leiby <nathan.leiby@clever.com>
2016-02-12 22:37:28 +0000

    `awslogs get`: inform user if no streams match pattern
    Resolves:
    https://github.com/jorgebastida/awslogs/issues/52
    
    Previously, given a query like:
    ```
    $ python run.py get my-log-group my-pattern.*
    ```
    
    if no streams matched `my-pattern.*` then results from all streams would
    be returned.
    
    Now the user sees an error message if no streams match their pattern:
    
    ```
    $ python run.py get my-log-group my-pattern.*
    No streams match your pattern 'my-pattern.*'.
    ```

--- awslogs/exceptions.py
+++ awslogs/exceptions.py
@@ -19,7 +19,15 @@ class TooManyStreamsFilteredError(BaseAWSLogsException):
     code = 6
 
     def hint(self):
-        return ("The number of streams that match your patter '{0}' is '{1}'. "
+        return ("The number of streams that match your pattern '{0}' is '{1}'. "
                 "AWS API limits the number of streams you can filter by to {2}."
                 "It might be helpful to you to not filter streams by any "
                 "pattern and filter the output of awslogs.").format(*self.args)
+
+
+class NoStreamsFilteredError(BaseAWSLogsException):
+
+    code = 7
+
+    def hint(self):
+        return ("No streams match your pattern '{0}'.").format(*self.args)
