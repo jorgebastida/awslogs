60dc53f6c8b068d128505460e376fafc5e98955e
Jorge Bastida <me@jorgebastida.com>
2016-03-27 12:04:24 +0200

    Remove pbr but keep tox

--- setup.py
+++ setup.py
@@ -1,3 +1,45 @@
-from setuptools import setup
+import sys
 
-setup(setup_requires=['pbr>=1.8.1'], pbr=True)
+from setuptools import setup, find_packages
+
+install_requires = [
+    'boto3>=1.2.1',
+    'termcolor>=1.1.0',
+    'python-dateutil>=2.4.0'
+]
+
+# as of Python >= 2.7 argparse module is maintained within Python.
+if sys.version_info < (2, 7):
+    install_requires.append('argparse>=1.1.0')
+
+setup(
+    name='awslogs',
+    version='0.4.0',
+    url='http://github.com/jorgebastida/awslogs',
+    license='BSD',
+    author='Jorge Bastida',
+    author_email='me@jorgebastida.com',
+    description='awslogs is a simple command line tool to read aws cloudwatch logs.',
+    long_description='awslogs is a simple command line tool to read aws cloudwatch logs.',
+    keywords="aws logs cloudwatch",
+    packages=find_packages(),
+    platforms='any',
+    install_requires=install_requires,
+    test_suite='tests',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+    ],
+    entry_points={
+        'console_scripts': [
+            'awslogs = awslogs.bin:main',
+        ]
+    },
+    zip_safe=False
+)
